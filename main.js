18ed0388c653898c3613ba8803ec7ab32a297faa
Jose Murillo <jooseemurillo@gmail.com>
2019-11-20 22:57:28 -0300

    promises API GITHUB

--- main.js
+++ main.js
@@ -1,11 +1,41 @@
-var xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'https://api.github.com/users/JoseMurilloc');
-xhr.send(null);
+var minhaPromise = function() {
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.github.com/users/JoseMurilloc');
+    xhr.send(null);
 
-xhr.onreadystatechange = function() {
-  if(xhr.readyState === 4) {
-    console.log(JSON.parse(xhr.responseText));
-    
-  }
+    xhr.onreadystatechange = function() {
+      if(xhr.readyState === 4) {
+        if(xhr.status === 200) {
+          resolve(JSON.parse(xhr.responseText));
+        } else {
+          reject('Erro na requisição');
+        }
+      }
+    }
+  
+  });
 }
+
+
+minhaPromise()
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(error) {
+    console.warn(error);
+  })
+
+
+// var xhr = new XMLHttpRequest();
+
+// xhr.open('GET', 'https://api.github.com/users/JoseMurilloc');
+// xhr.send(null);
+
+// xhr.onreadystatechange = function() {
+//   if(xhr.readyState === 4) {
+//     console.log(JSON.parse(xhr.responseText));
+    
+//   }
+// }
